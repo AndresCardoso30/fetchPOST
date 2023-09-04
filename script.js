252dfa28cef9e1fd3335600dd2e7b34c9f294fbd
Andres Cardoso <andress.cardosoo@gmail.com>
2023-09-04 16:07:34 -0300

    primer commit

---
+++ script.js
@@ -0,0 +1,23 @@
+
+const boton = document.getElementById('enviar')
+var url = "https://jsonplaceholder.typicode.com/users";
+var data = {
+        nombre : document.getElementById("nombre").value,
+        apellido: document.getElementById("apellido").value,
+        fecha_nacimiento: document.getElementById("fecha").value,
+};
+    
+function guardarInfo(){    
+fetch(url, {
+    method: "POST", 
+    body: JSON.stringify(data), 
+    headers: {
+    "Content-Type": "application/json",
+    },
+})
+    .then((res) => res.json())
+    .catch((error) => console.error("Error:", error))
+    .then((response) => console.log("Success:", response));
+}
+
+boton.addEventListener('click', guardarInfo)
